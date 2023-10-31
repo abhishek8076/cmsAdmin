d6cf135e42aa34528bbd20db9f2bad23660e2237
Abhishek <user@DESKTOP-ASH69UE>
2023-10-31 12:29:42 +0530

    dfghj

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App.js';
 import $ from "jquery";
-// import './index.css'
-// import './assets/vendor/apexcharts/apexcharts.css'
-// import './assets/vendor/bootstrap/css/bootstrap.min.css'
-// import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
-// import './assets/vendor/quill/quill.snow.css'
-// import './assets/vendor/quill/quill.bubble.css'
-// import './assets/vendor/remixicon/remixicon.css'
-// import './assets/vendor/simple-datatables/style.css'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/js/bootstrap.js';
 import { BrowserRouter } from 'react-router-dom';
 
